ac3213ac16811e4a6ff4d2e665d0f35221e43e59
taeseok <tsk941229@gmail.com>
2025-07-17 08:34:39 +0900

    feat: HttpServletResponse 객체 실습

--- src/main/java/com/edu/test/SecondServlet.java
+++ src/main/java/com/edu/test/SecondServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/second")
 public class SecondServlet extends HttpServlet {
@@ -14,5 +15,12 @@ public class SecondServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("SecondServlet!!");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head></head><title>test</title></head>");
+        out.println("<body><h1>have a nice day!!</h1></body>");
+        out.println("</html>");
+        out.close();
+
     }
 }
