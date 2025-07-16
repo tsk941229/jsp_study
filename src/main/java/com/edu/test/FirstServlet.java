2a6eb9cb9c87c60bfddb1bcdb170e57f20fec2a8
taeseok <tsk941229@gmail.com>
2025-07-16 22:43:44 +0900

    feat: Servlet 실습

--- src/main/java/com/edu/test/FirstServlet.java
+++ src/main/java/com/edu/test/FirstServlet.java
@@ -1,9 +1,24 @@
 package com.edu.test;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/hello2")
 public class FirstServlet extends HttpServlet {
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init() 실행됨!");
+        System.out.println("config = " + config);
+    }
 
-
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("service() 실행됨!");
+    }
 }
