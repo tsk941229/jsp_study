85b8f322e9134068c3b9ff8ee53b0c7dbacea65b
taeseok <tsk941229@gmail.com>
2025-07-23 22:15:10 +0900

    feat: Filter 실습

--- src/main/java/com/edu/test/FlowFilterTwo.java
+++ src/main/java/com/edu/test/FlowFilterTwo.java
@@ -5,12 +5,20 @@ import java.io.IOException;
 
 public class FlowFilterTwo implements Filter {
 
+    String charset;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         System.out.println("init() 호출 ... two");
+
+        charset = filterConfig.getInitParameter("en");
+
+
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+
+        req.setCharacterEncoding(charset);
 
         System.out.println("doFilter() 호출 전 ... two");
 
---
+++ src/main/java/com/edu/test/Output3Servlet.java
@@ -0,0 +1,24 @@
+package com.edu.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/output3")
+public class Output3Servlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+
+        out.println(req.getParameter("subject"));
+        out.close();
+
+    }
+}
